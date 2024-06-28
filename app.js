72e257795374d9d85e40122ad593e6dc82e6719f
VijayAbba <vijaykrishna.ed@gmail.com>
2024-06-28 19:42:50 +0530

    some-work-done

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get("/people", async (req, res) => {
   }
 });
 
-mongoose.connect("mongodb://localhost:27017/swfavorites", { useNewUrlParser: true }, (err) => {
+mongoose.connect("mongodb://mongodb:27017/swfavorites", { useNewUrlParser: true }, (err) => {
   if (err) {
     console.log(err);
   } else {
